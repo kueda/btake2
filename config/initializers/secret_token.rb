3b432ec236b975eb573f5ff7c0e6252412ef4d42
Ken-ichi Ueda <kenichi.ueda@gmail.com>
2013-05-28 14:02:28 -0700

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Btake2::Application.config.secret_token = '32ded79bf32582b02711f080e5510b7b985e0c5002fbd5f55f43693df2fc1964e9795073c5466962ec99fcb6cd6e9201225f01ca850ec61858f24a6f3c82d6ae'
